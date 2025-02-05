6ca6c9f23b5220c81f04430eac9fc2959d75d4fb
Alban30so <72690994+Alban30so@users.noreply.github.com>
2025-02-05 08:53:47 +0100

    HSV

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import cv2 as cv
 boules=cv.imread("ressources/images/boules.png")
 cv.imshow("boules",boules)
 cv.waitKey(0)
-cv.destroyAllWindows()
 
 #Question 10 : utilisation de shape et affichage du tableau numpy qui contient l'image
 print("Question 10 : ")
@@ -13,5 +12,28 @@ print(f"Tableau numpy de l'image \n: {boules}")
 # Récupération des trois canaux RGB de l'image
 r_boules, g_boules, b_boules = cv.split(boules)
 # Affichage du rouge
+
 cv.imshow("Boules (Rouge)", r_boules)
 cv.waitKey(0)
+#HSV
+# Conversion BGR -> HSV
+hsv_boules = cv.cvtColor(boules, cv.COLOR_BGR2HSV)
+cv.imshow("Boules (HSV)", hsv_boules)
+cv.waitKey(0)
+# Extraction des canaux HSV
+hue, saturation, value = cv.split(hsv_boules)
+# Intervalle teinte rouge
+I = 10
+min_rouge1 = (0, 50, 50)
+max_rouge1 = (I, 255, 255)
+min_rouge2 = (180-I, 50, 50)
+max_rouge2 = (180, 255, 255)
+# Masques des deux intervalles
+masque1 = cv.inRange(hsv_boules, min_rouge1, max_rouge1)
+masque2 = cv.inRange(hsv_boules, min_rouge2, max_rouge2)
+# Fusion des masques
+masque_rouge = cv.bitwise_or(masque1, masque2)
+cv.imshow("masque rouge", masque_rouge)
+cv.waitKey(0)
+
+#Avec imShow, afficher l’image correspondant à ce dernier tableau : les pixels blancs correspondent à ceux dont la valeur de teinte est proche de zéro.
