082abf3ba9718bcc40e9f780bcca85306a596cf0
Alban30so <72690994+Alban30so@users.noreply.github.com>
2025-02-05 08:28:09 +0100

    question 10

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
-from exos.exo1 import exercice1
-from exos.exo2 import exercice2
-from exos.exo3 import exercice3
-from exos.traitement import traitement1, traitement2, traitement3
-from exos.contour import contour
+import numpy as np
+import cv2 as cv
+boules=cv.imread("ressources/images/boules.png")
+cv.imshow("boules",boules)
+cv.waitKey(0)
+cv.destroyAllWindows()
 
-# import cv2 as cv
-
-def main():
-    print("Début du programme principal")
-    
-    # Appel des fonctions des exercices
-    print("\nExercice 1 : ")
-    exercice1()
-    print("\nExercice 2 : ")
-    exercice2()
-    print("\nExercice 3 : ")
-    exercice3()
-    print("\nExercice 4 : ")
-    image_binary = traitement1()
-    tmp=traitement2()
-    tmp2 = traitement3(tmp)
-    contour(image_binary)
-
-
-    print("Fin du programme principal")
-
-if __name__ == "__main__":
-    main()
+#Question 10 : utilisation de shape et affichage du tableau numpy qui contient l'image
+print("Question 10 : ")
+print(f"Dimensions de l'image : {boules.shape}")
+print(f"Tableau numpy de l'image \n: {boules}")
